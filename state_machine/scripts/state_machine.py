e5b1eac5486f41f728a7c1f4e7308c4db6021408
Landzs <landzs@hotmail.com>
2018-10-21 19:48:29 +0200

    Update State Machine

--- state_machine/scripts/state_machine.py
+++ state_machine/scripts/state_machine.py
@@ -15,14 +15,19 @@ from nav_msgs.msg import Odometry
 import itertools
 import tf
 import time
+import threading
 #from arduino_servo_control.srv import *
+GRIP = False
+START = False
+
+
+
 #####################################################
 #                  Initialization                   #
 #####################################################
 class Initialization(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['Initialization Done'])
-        self.counter = 0
 
     def execute(self, userdata):
         rospy.loginfo('Executing state Initialization')
@@ -36,20 +41,77 @@ class Standby(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['Receive Start Message'])
 
+        
     def execute(self, userdata):
+        global START
         rospy.loginfo('Executing state Standby')
+        while not START:
+            pass
         return 'Receive Start Message'
+
+
+
         
 #####################################################
-#                    Standby                       #
+#                Look For Object                   #
 #####################################################
-class Standby(smach.State):
+class Look_For_Object(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['Receive Start Message'])
+        smach.State.__init__(self, outcomes=['Detected Object'])
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state Standby')
-        return 'Receive Start Message'
+        rospy.loginfo('Executing state Look_For_Object')
+        return 'Detected Object'
+
+
+#####################################################
+#                 Follow_Path                       #
+#####################################################
+class Follow_Path(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['Reached Gripping Target','Reached Releasing Target'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state Follow_Path')
+        if not GRIP:
+            return 'Reached Gripping Target'
+        else:
+            return 'Reached Releasing Target'
+
+#####################################################
+#                 Grip_Object                       #
+#####################################################
+class Grip_Object(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['Gripped Object'])
+
+    def execute(self, userdata):
+        global GRIP
+        rospy.loginfo('Executing state Grip_Object')
+        GRIP = True
+        return 'Gripped Object'
+
+#####################################################
+#                Release_Object                     #
+#####################################################
+class Release_Object(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['Released Object'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state Release_Object')
+        return 'Released Object'
+#####################################################
+#                  Feedback_Start                   #
+#####################################################
+def feedback_start(msg):
+    global START
+    print("#")
+    if msg.data == True:
+        START = True
+        print(START)
+    else :
+        pass
 
 
 
@@ -60,17 +122,30 @@ def main():
     sm = smach.StateMachine(outcomes=['Stop'])
     sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
     sis.start()
+    rospy.Subscriber('/Start', std_msgs.msg.Bool, feedback_start)
     # Open the container
     with sm:
         # Add states to the container
         smach.StateMachine.add('Initialization', Initialization(), 
                                transitions={'Initialization Done':'Standby'})
-        smach.StateMachine.add('Standby', Standby(), 
-                               transitions={'Receive Start Message':'Stop'})
 
+        smach.StateMachine.add('Standby', Standby(), 
+                               transitions={'Receive Start Message':'Look_For_Object'})
+
+        smach.StateMachine.add('Look_For_Object', Look_For_Object(), 
+                               transitions={'Detected Object':'Follow_Path'})
+
+        smach.StateMachine.add('Follow_Path', Follow_Path(), 
+                               transitions={'Reached Gripping Target':'Grip_Object', 'Reached Releasing Target':'Release_Object'})
+
+        smach.StateMachine.add('Grip_Object', Grip_Object(), 
+                               transitions={'Gripped Object':'Follow_Path'})
+                               
+        smach.StateMachine.add('Release_Object', Release_Object(), 
+                               transitions={'Released Object':'Stop'})                             
     # Execute SMACH plan
     outcome = sm.execute()
-    rospy.spin()
+    #rospy.spin()
     sis.stop()
 
 
